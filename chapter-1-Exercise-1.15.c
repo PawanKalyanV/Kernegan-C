9b1a126e7702925d6857a762ad60b8316f158c8c
PawanKalyanV <52121882+PawanKalyanV@users.noreply.github.com>
2020-05-24 09:54:00 +0530

    Create chapter-1-Exercise-1.15.c

---
+++ chapter-1-Exercise-1.15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int fun1(celcius,fahr)
+  {
+
+    celcius = ((5/9)*(fahr-32));
+  }
+int main()
+ {
+     int celcius,fahr,lowr=0,uppr=300,step=20;
+     fahr=lowr;
+     printf("celcius   fahrenheit\n");
+     while(fahr <= uppr)
+    {
+       fun1(celcius,fahr);
+       printf("%d   %d\n",celcius, fahr);
+       fahr=fahr+step;
+    }return 0;
+ }
