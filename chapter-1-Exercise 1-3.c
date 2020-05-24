892dc173b102c5a1322f2c3b3b3406c5ffd2cd1f
PawanKalyanV <52121882+PawanKalyanV@users.noreply.github.com>
2020-05-24 09:38:07 +0530

    Create chapter-1-Exercise 1-3.c

---
+++ chapter-1-Exercise 1-3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float celcius,fahrn;
+    int lowr=0,uppr=300,step=20;
+    fahrn=lowr;
+    printf("celcius    fahrenhiet \n");
+    while(fahrn<=uppr)
+    {
+        celcius=(5.0/9.0)*(fahrn-32);
+        printf("%.2f      %.2f\n",celcius,fahrn);
+        fahrn=fahrn+step;
+
+    }
+    return 0;
+}
