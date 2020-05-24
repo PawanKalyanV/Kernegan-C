201ed95d728a437a48be35bbdc433cd010fe3373
PawanKalyanV <52121882+PawanKalyanV@users.noreply.github.com>
2020-05-24 09:53:07 +0530

    Create chapter-1-Exercise-1.13.c

---
+++ chapter-1-Exercise-1.13.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int c;
+    while( (c = getchar() ) != EOF)
+    {
+        if (c == ' ' )
+        putchar('\n');
+        else
+        putchar('|');
+    } return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int c;
+    while ((c = getchar()) != EOF )
+    {
+
+     if (c == ' ')
+        putchar(' ');
+     else
+        putchar('_');
+        putchar('\n');
+    }return 0;
+}
