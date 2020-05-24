1dfbbd4b9ba1340a70f8c4782fb2e0af5d8343e5
psmurthy <ramji.423@gmail.com>
2020-05-24 10:14:26 +0530

    Indentation corrections

--- chapter-1-exercise-1.17.c
+++ chapter-1-exercise-1.17.c
@@ -1,14 +1,18 @@
 /* Prints all input lines greater that are longer than 80 characters. */
+
 #include<stdio.h>
+
 #define MAXLINE 1000
-int getline(char line[], int maxline);
+
+int getLine(char line[], int maxline);
+
 int main()
 {
     int len;
     int max=80;
     char line[MAXLINE];
     
-    while ((len = getline(line, MAXLINE)) > 0)
+    while ((len = getLine(line, MAXLINE)) > 0)
         if (len >= max)
         {
           printf( "%s", line);
@@ -16,17 +20,19 @@ int main()
         }
 }
 
-int getline(char s[], int lim)
+int getLine(char s[], int lim)
 {
     int c, i;
-    for (i=0; i<lim-1 && (c = getchar()) != EOF && c !='\n'; ++i)
+
+    for (i=0; i < lim-1 && (c = getchar()) != EOF && c !='\n'; ++i)
          s[i] = c;
     if (c == '\n')
     {
         s[i] = c;
          ++i;
     }
+
     s[i]= '\0';
-        return i;
+    return i;
 }
 
