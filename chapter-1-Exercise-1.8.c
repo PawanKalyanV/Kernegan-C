d53e5a14552f6250b340711dc3eaca4a9de1960e
PawanKalyanV <52121882+PawanKalyanV@users.noreply.github.com>
2020-05-24 09:45:30 +0530

    Create chapter-1-Exercise-1.8.c

---
+++ chapter-1-Exercise-1.8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int c,blank=0,tab=0,newline=0;
+    while((c=getchar() !=EOF))
+          if(c=='\n')
+            ++newline;
+          if(c=='\t')
+            ++tab;
+          if(c==' ')
+            ++blank;
+        printf("newlines=%d  tabs=%d  blanks=%d",newline,tab,blank);
+        return 0;
+
+}
