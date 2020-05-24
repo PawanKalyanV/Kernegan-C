dcde2af67ca53a6a17f39bc118809cb37bf053c6
PawanKalyanV <52121882+PawanKalyanV@users.noreply.github.com>
2020-05-24 09:48:53 +0530

    Create chapter-1-Exercise-1.9.c

---
+++ chapter-1-Exercise-1.9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int c, end_c; /* end character */
+    end_c=c; /* we initialize character in end char for comparision */
+    while (( c = getchar() ) != EOF )
+    {
+        if(c == ' ')
+        {
+          if (end_c != ' ') /* more than one space simply print char */
+           putchar(c);
+        }
+        else
+            putchar(c); /*  simply print char until we enter space */
+            end_c=c; /* end of the character should store in end_c for comparision  of next char we entered */
+
+
+    }return 0;
+}
