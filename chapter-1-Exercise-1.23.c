bde24af36f63195ff10ac9c55b5c2caa477f1c12
PawanKalyanV <52121882+PawanKalyanV@users.noreply.github.com>
2020-05-29 18:26:49 +0530

    Create chapter-1-Exercise-1.23.c

---
+++ chapter-1-Exercise-1.23.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void comment();
+int main()
+{
+    int c;
+    while((c = getchar()) != EOF)
+    {
+        if (c == '/')
+        if (c == '*')
+        if (c == '/' && c=='/')
+            comment();
+
+    }return 0;
+}
+void comment()
+{
+    int c;
+    while (c != '*' && c != '/')
+    {
+        c=getchar();
+    }
+
+}
