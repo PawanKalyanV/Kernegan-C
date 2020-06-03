5de3ff2578f9b3c7ac7551f03ea65de09a01a176
psmurthy <ramji.423@gmail.com>
2020-06-03 13:54:49 +0530

    Indentation corrections

--- chapter-1/E23.c
+++ chapter-1/E23.c
@@ -1,10 +1,10 @@
 
-/* * Remove comments from program */
-// * printf("/* hi */");
-// * /*hi */
-// * a=a/b
-// * char a= '\"'
-// * /* comment */
+/* Remove comments from program */
+// printf("/* hi */");
+// /*hi */
+// a=a/b
+// char a= '\"'
+// /* comment */
 
 #include<stdio.h>
 
@@ -18,72 +18,78 @@ int main()
 {
     int c, d;
 
-    while (( c = getchar() ) != EOF )
+    while ((c = getchar()) != EOF)
     {
-       if (c == '"')
-       {
-	  putchar(c);
-           in_doublequote();
-       }
-       else if ( c == '\'')
-       {
-         putchar(c);
-	 in_singlequote();
-       }
-        else if (c == '/')
-	{
-        d= getchar();
-        if (d == '*')
-           in_comment();
-       else
-       {
-         putchar(c);
- 	 putchar(d);
-	}
-	}
-       else
-       {
-	  putchar(c);
-       }
-    } 
-    return 0;
+        if (c == '"')
+        {
+            putchar(c);
+            in_doublequote();
+        }
+        else if (c == '\'')
+        {
+            putchar(c);
+            in_singlequote();
+        }
+        else if (c == '/') {
+            d= getchar();
+            if (d == '*')
+            {
+                in_comment();
+            }
+            else
+            {
+                putchar(c);
+                putchar(d);
+            }
+        }
+        else
+        {
+            putchar(c);
+        }
+   } 
+   return 0;
 }
 
+/* Ignore all characters with in the comment. */
 void in_comment()
 {
     int c, i;
+
     c = getchar();
     i = getchar();
 
     while (c != '*' && i != '/')
     {
-	c = i;
-	i = getchar();
+        c = i;
+        i = getchar();
     }
-
 }
 
+/* Output all character with in the double quotes. */
 void in_doublequote()
 {
-     int c;
-     c = getchar();
+    int c;
 
-     while (c != '"')
-     {
-      putchar(c);
-     c = getchar();
-     }
-     putchar(c); 
+    c = getchar();
+
+    while (c != '"')
+    {
+        putchar(c);
+        c = getchar();
+    }
+    putchar(c); 
 }
 
+/* Output all character with in the single quotes. */
 void in_singlequote()
 {
-	int c;
-	c = getchar();
-	while (c != '\'')
-        {
-	 putchar(c);
-	 c = getchar();
-	}
-	putchar(c);
+    int c;
+
+    c = getchar();
+    while (c != '\'')
+    {
+        putchar(c);
+        c = getchar();
+    }
+    putchar(c);
 }
