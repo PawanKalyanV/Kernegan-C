9b9f8bf037b733d35ba498e01a68a60321dd913a
pawankalyanV <pavankalyan4070@gmail.com>
2020-06-13 18:02:49 +0530

    new

---
+++ chapter-1/noncontiguous.c
@@ -0,0 +1,20 @@
+
+
+#include<stdio.h>
+
+int main()
+{
+  int c, n=0, count, end_c;
+
+ while (( c = getchar() ) != EOF )
+  
+      if ( c == ' ' )
+       {
+	 {
+          if ( end_c == ' ' )
+           n++;
+         }
+       }
+       printf("%d",n);
+       end_c = c;
+}
